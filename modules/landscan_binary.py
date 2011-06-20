ff061858a08d93aaaea52036a36a03ae50d7b2ba
nielso <nielso@8803295c-9095-4c5a-8e22-6fb375fdcc3b>
2011-06-20 05:17:56 +0000

    Updated code to use original landscan data
    git-svn-id: http://www.aifdr.org/svn/impact_map@14 8803295c-9095-4c5a-8e22-6fb375fdcc3b

--- modules/landscan_binary.py
+++ modules/landscan_binary.py
@@ -3,7 +3,8 @@ import numpy as np
 import os
 
 library_dir = os.environ['IMPACTLIB']
-pop_file = os.path.join(library_dir, 'population', 'landscan2008.asc')
+pop_dir = os.path.join(library_dir, 'population')
+pop_file = os.path.join(pop_dir, 'landscan.asc')
 
 ls = np.loadtxt(pop_file, dtype='float', skiprows=6)
 
@@ -17,11 +18,11 @@ n_bound = s_bound + nrows*dl
 landscan_info = {'w_bound': w_bound,'s_bound': s_bound,'e_bound': e_bound,\
                  'n_bound': n_bound,'step': dl}
 
-output1 = open('landscan_binary.pkl','wb')
+output1 = open(os.path.join(pop_dir, 'landscan_binary.pkl'), 'wb')
 pickle.dump(ls, output1)
 output1.close()
 
-output2 = open('landscan_info.pkl','wb')
+output2 = open(os.path.join(pop_dir, 'landscan_info.pkl'), 'wb')
 pickle.dump(landscan_info, output2)
 
 output2.close()
