33c2223b619254c7b862682d121ef6884067e523
nielso <nielso@8803295c-9095-4c5a-8e22-6fb375fdcc3b>
2011-06-20 02:22:42 +0000

    Make pkl file
    git-svn-id: http://www.aifdr.org/svn/impact_map@11 8803295c-9095-4c5a-8e22-6fb375fdcc3b

--- modules/landscan_binary.py
+++ modules/landscan_binary.py
@@ -1,6 +1,11 @@
 import pickle
 import numpy as np
-##ls = np.loadtxt('landscan.asc',dtype='float',skiprows=6)
+import os
+
+library_dir = os.environ['IMPACTLIB']
+pop_file = os.path.join(library_dir, 'population', 'landscan2008.asc')
+
+ls = np.loadtxt(pop_file, dtype='float', skiprows=6)
 
 w_bound = 94.972335
 s_bound = -11.009721
@@ -12,9 +17,9 @@ n_bound = s_bound + nrows*dl
 landscan_info = {'w_bound': w_bound,'s_bound': s_bound,'e_bound': e_bound,\
                  'n_bound': n_bound,'step': dl}
 
-##output1 = open('landscan_binary.pkl','wb')
-##pickle.dump(ls, output1)
-##output1.close()
+output1 = open('landscan_binary.pkl','wb')
+pickle.dump(ls, output1)
+output1.close()
 
 output2 = open('landscan_info.pkl','wb')
 pickle.dump(landscan_info, output2)
