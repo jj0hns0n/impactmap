444698e0803d7383d788c46777e9d95da04f10fb
nielso <nielso@8803295c-9095-4c5a-8e22-6fb375fdcc3b>
2011-06-28 07:06:19 +0000

    Cleaning up
    git-svn-id: http://www.aifdr.org/svn/impact_map@29 8803295c-9095-4c5a-8e22-6fb375fdcc3b

--- modules/event_info.py
+++ modules/event_info.py
@@ -21,16 +21,19 @@ def event_info(shakedata_dir, event_name):
 
     line = line.split(' ')
 
-    event_info = {'mag': line[1],'lat': line[2],'lon': line[3],'month': line[4],\
-                  'day': line[5],'year': line[6],'time': line[7],'time-zone': \
-                  line[8],'w_bound': line[9],'s_bound': line[10],'e_bound':\
-                  line[11],'n_bound': line[12]}
-
-
+    event_info = {'mag': line[1],
+                  'lat': line[2],
+                  'lon': line[3],
+                  'month': line[4],
+                  'day': line[5],
+                  'year': line[6],
+                  'time': line[7],
+                  'time-zone': line[8],
+                  'w_bound': line[9],
+                  's_bound': line[10],
+                  'e_bound': line[11],
+                  'n_bound': line[12]}
 
     A = np.loadtxt(path, dtype=float, skiprows=1)
-    lon = A[:,0]
-    lat = A[:,1]
-    mi = A[:,4]
     return event_info, A
 
