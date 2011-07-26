1a1bf2765a6ce7df6351875a53c42b240eafb007
ghaseh <ghaseh@8803295c-9095-4c5a-8e22-6fb375fdcc3b>
2011-07-26 02:11:41 +0000

    code:broken
    git-svn-id: http://www.aifdr.org/svn/impact_map@67 8803295c-9095-4c5a-8e22-6fb375fdcc3b

--- modules/city_info.py
+++ modules/city_info.py
@@ -1,7 +1,8 @@
 import numpy as np
 from utilities import make_grid
+import points2distance
 
-def city_info(R, B, path):
+def city_info(R, B, path, eve_info):
     """List cities exposed sorted by MMI experienced
 
     Input
@@ -15,53 +16,57 @@ def city_info(R, B, path):
     A = np.loadtxt(path, dtype={'names': ('lon', 'lat', 'pop', 'name'),
                                 'formats': (float, float, float, 'S10')})
 
+    
+    print R[0],R[1],R[2],R[3]
     # Clip to region of interest
     index = np.nonzero((A['lon'] > R[0]) &
                        (A['lon'] < R[1]) &
                        (A['lat'] > R[2]) &
                        (A['lat'] < R[3]))
-    A = A[index]
-
+    
+    
+    if index:
+       eve_lon = float(eve_info['lon'])
+       eve_lat = float(eve_info['lat'])
+       dist_dum = 1e08
+       print dist_dum
+       for i in range(len(A)):
+           start = ((A['lon'][i],0,0),(A['lat'][i],0,0))
+           end = ((eve_lon,0,0),(eve_lat,0,0))
+           dis = points2distance.points2distance(start,end)
+           if dis<dist_dum:
+              index =i
+         
+       A = A[index]
+       print A
+       city = dict([('name',A['name']),('population',A['pop']),('intensity',0),('lon',A['lon']),('lat',A['lat'])])
+       city = np.array(city)
+       print 'length=', city, len(city)
+    else:
+       A = A[index]
     # Create grid of MMI values (FIXME: More explanation in here please)
-    points = B[:,0:2]
-    values = B[:,4]
-    I = make_grid(points, values, (A['lon'], A['lat']))
+       points = B[:,0:2]
+       values = B[:,4]
+       I = make_grid(points, values, (A['lon'], A['lat']))
 
     # Create city list with entries: Name, population, MMI, lon, lat
-
-    # FIXME (Ole): Hack until ticket:2 has been addressed
-    # if len(A) == 0:
-    #     # Dummy city
-    #     intensity = [0.0]
-    #     name = ['Kosong']
-    #     pop = [10000]
-    #     lon = [112.27]
-    #     lat = [-7.530]
-    # else:
-    #     intensity = I.tolist()
-    #     name = A['name'].tolist()
-    #     pop = A['pop'].tolist()
-    #     lon = A['lon'].tolist()
-    #     lat = A['lat'].tolist()
-
-    intensity = I.tolist()
-    name = A['name'].tolist()
-    pop = A['pop'].tolist()
-    lon = A['lon'].tolist()
-    lat = A['lat'].tolist()
-
-    city = zip(name, pop, intensity, lon, lat)
+       intensity = I.tolist()
+       name = A['name'].tolist()
+       pop = A['pop'].tolist()
+       lon = A['lon'].tolist()
+       lat = A['lat'].tolist()
+       city = zip(name, pop, intensity, lon, lat)
 
     # Convert to array and sort by intensity
     # (and population if intensity is the same)
-    dtype = [('name','S10'),
+       dtype = [('name','S10'),
              ('population',float),
              ('intensity',float),
              ('lon',float),
              ('lat',float)]
-    city = np.array(city, dtype=dtype)
-    city = np.sort(city, order=['intensity', 'population'])
-    city = np.flipud(city)
+       city = np.array(city, dtype=dtype)
+       city = np.sort(city, order=['intensity', 'population'])
+       city = np.flipud(city)
 
     return city
 
