738704762fe9c6b23a8d13bfff7406897af85526
awinx <awinx@8803295c-9095-4c5a-8e22-6fb375fdcc3b>
2011-08-03 08:00:17 +0000

    Updated cron script to work at Pusdatin
    git-svn-id: http://www.aifdr.org/svn/impact_map@115 8803295c-9095-4c5a-8e22-6fb375fdcc3b

--- dampa_cron.sh
+++ dampa_cron.sh
@@ -3,8 +3,8 @@
 # Example of crontab entry
 #03 * * * * nielso cd /home/nielso/sandpit/impact_map; sh dampa_cron.sh > dampa_cron.log 2>&1
 
-. /home/nielso/shake/bin/activate
-export SHAKEDATA=/home/nielso/shakedata
-export IMPACTLIB=/home/nielso/impactlib
+. ~/shake_env/bin/activate
+export SHAKEDATA=~/shakedata
+export IMPACTLIB=~/impactlib
 python generate_impact_map.py
 python get_shakemap_data.py
