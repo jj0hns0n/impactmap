1d7df106f2293aabc318b9a5020f8e9c9ec39a3f
nielso <nielso@8803295c-9095-4c5a-8e22-6fb375fdcc3b>
2011-08-03 04:12:14 +0000

    Added example cron script
    git-svn-id: http://www.aifdr.org/svn/impact_map@111 8803295c-9095-4c5a-8e22-6fb375fdcc3b

---
+++ dampa_cron.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# Example of crontab entry
+#03 * * * * nielso cd /home/nielso/sandpit/impact_map; sh dampa_cron.sh > dampa_cron.log 2>&1
+
+. /home/nielso/shake/bin/activate
+export SHAKEDATA=/home/nielso/shakedata
+export IMPACTLIB=/home/nielso/impactlib
+python generate_impact_map.py
