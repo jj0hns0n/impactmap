1615b1af15920a76bb1a8b07415f48719dda9f7d
nielso <nielso@8803295c-9095-4c5a-8e22-6fb375fdcc3b>
2011-08-05 03:11:32 +0000

    Prettified formatting of city_table
    git-svn-id: http://www.aifdr.org/svn/impact_map@123 8803295c-9095-4c5a-8e22-6fb375fdcc3b

--- modules/city_table.py
+++ modules/city_table.py
@@ -22,7 +22,7 @@ def city_table(city_info, R, basename='comp2'):
     'D 0 1p'+'\n'+
     'N 3'+'\n'+
     'L '+ fontsize + ' '+ font +' C Kota'+'\n'+
-    'L '+ fontsize + ' '+ font +' C Penduduk'+'\n'+
+    'L '+ fontsize + ' '+ font +' C Penduduk (x1000)'+'\n'+
     'L '+ fontsize + ' '+ font +' C Intensitas'+'\n'+
     'D 0 1p'+'\n'+
     'END')
@@ -35,12 +35,15 @@ def city_table(city_info, R, basename='comp2'):
     N = min(len(city_info), 8)
     for i in xrange(N):
 
-        if city_info['population'][i]>1000:
-            pop = round(city_info['population'][i]/1000)
-            pop = "%0.0f"%pop+'k'
-        else:
-            # FIXME (Ole): pop may be unassigned here
-            pop = "%0.0f"%pop
+        # FIXME (Ole): Perhaps just report whole 1000's like in the table
+        #if city_info['population'][i]>1000:
+        #    pop = round(city_info['population'][i]/1000)
+        #    pop = '%0.0f' % pop+'k'
+        #else:
+        #    # FIXME (Ole): pop may be unassigned here
+        #    pop = '%0.0f' % pop
+        pop = round(city_info['population'][i]/1000)
+        pop = '%0.0f' % pop
 
         name = city_info['name'][i]
         color = rgb[int(city_info['intensity'][i])]
@@ -53,7 +56,7 @@ def city_table(city_info, R, basename='comp2'):
                   'L '+ fontsize + ' '+ font +' C '+MI+'\n'+
                   'D 0 1p'+'\n'+
                   'END')
-    os.system('pslegend -Dx5.15i/6.0i/2.75i/1.92i/TL '+J+' '+R+' -F -K cityinform.legend > %s.eps' % basename)
+    os.system('pslegend -Dx5.15i/6.0i/3.75i/1.92i/TL '+J+' '+R+' -F -K cityinform.legend > %s.eps' % basename)
     os.system('ps2pdf14 -dPDFSETTINGS=/prepress -dEPSCrop %s.eps' % basename)
     os.system('pdfcrop %s.pdf %s.pdf > logs/pdfcrop_%s.log' % (basename, basename, basename))
     os.system('rm cityinform.legend')
